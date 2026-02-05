1e96f0b212c3646861b904d291c8a2e408eccf4f
DaviRM <davi.morais3@fatec.sp.gov.br>
2026-02-05 20:02:52 -0300

    Main View

--- script/compile.sh
+++ script/compile.sh
@@ -3,6 +3,15 @@ JAVAFX_PATH="/home/davirm/Documentos/java-libs/javafx-sdk-25.0.1/lib/"
 echo "Rebuilding directories..."
 rm -rf dist && mkdir dist &&
 echo "Compiling..."
-javac --module-path "$JAVAFX_PATH" --add-modules javafx.controls -d ./dist/ ./src/application/*.java
+javac   --module-path "$JAVAFX_PATH" \
+        --add-modules javafx.controls,javafx.fxml  \
+        -d ./dist/ ./src/application/*.java
+echo "Creating some directories..."
+mkdir ./dist/gui/
+echo "Copying resources..."
+cp ./src/gui/*.fxml ./dist/gui/
 echo "Executing..."
-java --module-path "$JAVAFX_PATH" --add-modules javafx.controls -cp ./dist application.Main
+java    --module-path "$JAVAFX_PATH" \
+        --add-modules javafx.controls,javafx.fxml \
+        --add-opens gui/gui=MainView.fxml \
+        -cp ./dist application.Main
--- src/application/Main.java
+++ src/application/Main.java
@@ -1,18 +1,33 @@
 package application;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     public void start(Stage primaryStage) {
         try {
-            BorderPane root = new BorderPane();
-            Scene scene = new Scene(root, 400, 400);
-            primaryStage.setScene(scene);
+            /*
+                BorderPane root = new BorderPane();
+                Scene scene = new Scene(root, 400, 400);
+                Label label = new Label("My JavaFX Program");
+                root.setCenter(label);
+                primaryStage.setTitle("JavaFX on my Linux");
+                primaryStage.setScene(scene);
+                primaryStage.show();
+            */
+
+            Parent parent = new FXMLLoader(getClass().getResource("/gui/MainView.fxml")).load();
+
+            Scene mainScene = new Scene(parent);
+            primaryStage.setScene(mainScene);
+            primaryStage.setTitle("JavaFX on my Linux");
             primaryStage.show();
-        } catch (Exception e) {
+        } catch (IOException e) {
             System.out.println(e.getMessage());
         }
     }
